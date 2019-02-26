00a81c0c3fd4bce06ef8d4c9e4b1ed5266b2f89b
alejclemus <38306162+alejclemus@users.noreply.github.com>
2019-02-26 13:56:05 -0600

    Add files via upload

---
+++ Queue/src/com/company/Worker.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Worker {
+  int socialSecurityNumber;
+  String name;
+  int daysWorked;
+  String company;
+
+  Worker(int s,String n,int d, String c){
+      socialSecurityNumber=s;
+      name=n;
+      daysWorked=d;
+      company=c;
+  }
+}
