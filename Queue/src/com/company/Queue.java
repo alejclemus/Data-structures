57a3a9e5a445f65808acdbb07ec396a405355531
alejclemus <38306162+alejclemus@users.noreply.github.com>
2019-03-05 12:37:10 -0600

    Add files via upload

--- Queue/src/com/company/Queue.java
+++ Queue/src/com/company/Queue.java
@@ -3,7 +3,7 @@ package com.company;
 class Queue {
     int front, rear, size;
     int  capacity;
-    int array[];
+    Worker array[];
 
     Queue(int capacity) {
         this.capacity = capacity;
@@ -18,24 +18,23 @@ class Queue {
     boolean isEmpty(Queue queue)
     {  return (queue.size == 0); }
 
-    void add(int item)
+    void add(Worker newWorker)
     {
         if (isFull(this))
             return;
         this.rear = (this.rear + 1)%this.capacity;
-        this.array[this.rear] = item;
+        this.array[this.rear] = newWorker;
         this.size = this.size + 1;
-        System.out.println(item+ " enqueued to queue");
     }
 
-    int delete()
+    Worker delete()
     {
         if (isEmpty(this))
             return Integer.MIN_VALUE;
 
-        int item = this.array[this.front];
+        Worker newWorker = this.array[this.front];
         this.front = (this.front + 1)%this.capacity;
         this.size = this.size - 1;
-        return item;
+        return newWorker;
     }
 }
