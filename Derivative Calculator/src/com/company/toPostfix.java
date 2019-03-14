81e6eb572309a884f33772e2cce093039de367c9
alejclemus <38306162+alejclemus@users.noreply.github.com>
2019-03-14 10:58:02 -0600

    Add files via upload

--- Derivative Calculator/src/com/company/toPostfix.java
+++ Derivative Calculator/src/com/company/toPostfix.java
@@ -25,33 +25,35 @@ public class toPostfix {
     public String convert(String infix){
         System.out.printf("%-8s%-10s%-15s\n", "Input","Stack","Postfix");
         String postfix = "";
-        Stack<Character> s = new Stack<>();
-        s.push('#');
+        Stack s = new Stack();
+        Node node, symbol;
+        node=new Node('#');
+        s.push(node);
 
         System.out.printf("%-8s%-10s%-15s\n", "",format(s.toString()),postfix);
 
         for(int i = 0; i < infix.length(); i++){
-            char inputSymbol = infix.charAt(i);  //symbol to be processed
-            if(isOperator(inputSymbol)){  //if a operator
-                //repeatedly pops if stack top has same or higher precedence
-                while(checkPrecedence(inputSymbol, s.peek()))
+            char inputSymbol = infix.charAt(i);
+            if(isOperator(inputSymbol)){  //if (isOperator)
+                while(checkPrecedence(inputSymbol, s.peek().value))
                     postfix += s.pop();
-                s.push(inputSymbol);
+                symbol=new Node(inputSymbol);
+                s.push(symbol);
             }
             else if(inputSymbol == '(')
-                s.push(inputSymbol);  //push if left parenthesis
+            {symbol=new Node(inputSymbol);
+                s.push(symbol); } //if (isLeftParenthesis)
             else if(inputSymbol == ')'){
-                //repeatedly pops if right parenthesis until left parenthesis is found
-                while(s.peek() != '(')
+                while(s.peek().value != '(')
                     postfix += s.pop();
                 s.pop();
-            }
+            } //rightParenthesis
             else
                 postfix += inputSymbol;
             System.out.printf("%-8s%-10s%-15s\n", ""+inputSymbol,format(s.toString()),postfix);
         }
 
-        while(s.peek() != '#'){
+        while(s.peek().value != '#'){
             postfix += s.pop();
             System.out.printf("%-8s%-10s%-15s\n", "",format(s.toString()),postfix);
 
