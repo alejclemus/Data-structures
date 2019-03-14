c304fe11558f50e677276f178983127b20acb066
alejclemus <38306162+alejclemus@users.noreply.github.com>
2019-03-14 09:20:07 -0600

    Add files via upload

--- Derivative Calculator/src/com/company/ExpressionTree.java
+++ Derivative Calculator/src/com/company/ExpressionTree.java
@@ -11,8 +11,19 @@ public class ExpressionTree {
         return false;
     }
 
+    boolean isNumber(char c){
+        return (Character.isDigit(c));
+    }
+
     boolean isVariable(char c){
         return (Character.isLetter(c));
+    }
+
+    boolean isParenthesis(char c){
+        if(c== '(' || c== ')'){
+            return true;
+        }
+        return false;
     }
 
     void inorder(Node t) {
@@ -27,30 +38,38 @@ public class ExpressionTree {
         Stack nodeStack = new Stack();
         Node t, t1, t2;
 
+
         for (int i = 0; i < Expression.length; i++) {
 
-            if (!isOperator(Expression[i])) {
-                if (!isVariable(Expression[i]))
-                {
+            for(int j=0; j<Expression.length;j++){
+                if (isParenthesis(Expression[j])){
+
+
+                    }
+                }
+
+             if (isNumber(Expression[i])) {
                     t = new Node(Expression[i]);
                     nodeStack.push(t);
                 }
+            else if (isOperator(Expression[i])){
+                t = new Node(Expression[i]);
+                    //System.out.println(t.value);
+                    //System.out.println("/"+"\\");
+                t1 = nodeStack.pop();
+                t2 = nodeStack.pop();
+                t.left = t1;
+                    //System.out.print(t.left.value+" ");
+                t.right = t2;
+                    //System.out.print(t.right.value);
+                nodeStack.push(t);
+            }
+            else if(isParenthesis(Expression[i])){
 
             }
-            else{
-                t = new Node(Expression[i]);
-                if(!isVariable(Expression[i]))
-                {
-                    t1 = nodeStack.pop();
-                    t2 = nodeStack.pop();
-                    t.right = t1;
-                    t.left = t2;
-                }
-                else {
-                    
-                }
-
-                nodeStack.push(t);
+            else //if (isVariable)
+            {
+                i++;
             }
         }
         t = nodeStack.peek();
