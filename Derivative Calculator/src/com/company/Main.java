6676a90cf800f1d24e47e4b8b05ade36447c972c
alejclemus <38306162+alejclemus@users.noreply.github.com>
2019-03-14 08:07:00 -0600

    Add files via upload

--- Derivative Calculator/src/com/company/Main.java
+++ Derivative Calculator/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	ExpressionTree et=new ExpressionTree();
+	String expression="3x^2 * (x^2 + 1)^3 + e^2x";
+	char[] expressionarray=expression.toCharArray();
+	Node root=et.constructTree(expressionarray);
+	System.out.println("infix expression is");
+	et.inorder(root);
     }
 }
